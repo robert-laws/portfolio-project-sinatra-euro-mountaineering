07b0cfcfe2dc2a8732c4872ab850ef0aa230405d
Robert Laws <roblaws@me.com>
2017-11-08 15:52:11 +0300

    add slugifiable module and data for seeds

--- app/models/hiker.rb
+++ app/models/hiker.rb
@@ -1,6 +1,11 @@
+require_relative './concerns/slugifiable'
+
 class Hiker < ActiveRecord::Base
   belongs_to :club
   has_many :mountains, through: :hiker_mountains
+
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
 
   # has_secure_password
 end
