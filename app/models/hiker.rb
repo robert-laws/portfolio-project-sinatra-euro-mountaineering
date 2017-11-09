6212f8673bd38855e4c3b58f727d4edb092571e5
Robert Laws <roblaws@me.com>
2017-11-09 20:39:00 +0300

    updated models to all for the many-to-many associations to function properly

--- app/models/hiker.rb
+++ app/models/hiker.rb
@@ -2,6 +2,7 @@ require_relative './concerns/slugifiable'
 
 class Hiker < ActiveRecord::Base
   belongs_to :club
+  has_many :hiker_mountains
   has_many :mountains, through: :hiker_mountains
 
   extend Slugifiable::ClassMethods
--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -1,7 +1,9 @@
 require_relative './concerns/slugifiable'
 
 class Mountain < ActiveRecord::Base
+  has_many :hiker_mountains
   has_many :hikers, through: :hiker_mountains
+  has_many :clubs, through: :hikers
 
   extend Slugifiable::ClassMethods
   include Slugifiable::InstanceMethods
