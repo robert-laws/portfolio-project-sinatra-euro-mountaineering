47a006604710c95aa64241c3454fbf3a693f2cad
robert-laws <roblaws@me.com>
2017-11-10 11:42:23 +0300

    build signup form and begin validation and create/save hiker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
@@ -18,11 +20,35 @@ class ApplicationController < Sinatra::Base
     
   end
 
-  get "/account" do
-    erb :account
+  get "/signup" do
+    @valid = {fn: "", ln: "", em: "", un: "", pw: ""}
+    erb :signup
   end
 
-  post "/account" do
+  post "/signup" do
+    @valid = {fn: "", ln: "", em: "", un: "", pw: ""}
+    @result = false
+
+    params[:first_name] == "" ? @valid[:fn] = "* enter a valid first name" : @valid[:fn] = ""
+    params[:last_name] == "" ? @valid[:ln] = "* enter a valid last name" : @valid[:ln] = ""
+    params[:user_email] == "" ? @valid[:em] = "* enter a valid email" : @valid[:em] = ""
+    params[:username] == "" ? @valid[:un] = "* enter a valid username" : @valid[:un] == ""
+    params[:password] == "" ? @valid[:pw] = "* enter a valid password" : @valid[:pw] == ""
     
+    @valid.each do |k, v|
+      if v == ""
+        @result = true
+      else
+        @result = false
+        break
+      end
+    end
+
+    if @result
+      # create Hiker and save to database
+      erb :'hiker/show'
+    else
+      erb :signup
+    end
   end
 end
