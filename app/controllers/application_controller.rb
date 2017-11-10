11859910c357fb2f3e03ba63533a2a0da0aeee12
robert-laws <roblaws@me.com>
2017-11-10 15:43:27 +0300

    build signup, login, and logout features

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,11 +13,38 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/login" do
+    @valid = {un: "", pw: ""}
     erb :login
   end
 
   post "/login" do
-    
+    @valid = {un: "", pw: ""}
+    @result = false
+
+    params[:username] == "" ? @valid[:un] = "* enter a valid username" : @valid[:un] == ""
+    params[:password] == "" ? @valid[:pw] = "* enter a valid password" : @valid[:pw] == ""
+
+    @valid.each do |k, v|
+      if v == ""
+        @result = true
+      else
+        @result = false
+        break
+      end
+    end
+
+    if @result
+      # authenticate hiker
+      @hiker = Hiker.find_by(username: params[:username])
+      if @hiker && @hiker.authenticate(params[:password])
+        session[:hiker_id] = @hiker.id
+        redirect("/hikers/#{@hiker.id}")
+      else
+        erb :login
+      end
+    else
+      erb :login
+    end
   end
 
   get "/signup" do
@@ -31,7 +58,7 @@ class ApplicationController < Sinatra::Base
 
     params[:first_name] == "" ? @valid[:fn] = "* enter a valid first name" : @valid[:fn] = ""
     params[:last_name] == "" ? @valid[:ln] = "* enter a valid last name" : @valid[:ln] = ""
-    params[:user_email] == "" ? @valid[:em] = "* enter a valid email" : @valid[:em] = ""
+    params[:email] == "" ? @valid[:em] = "* enter a valid email" : @valid[:em] = ""
     params[:username] == "" ? @valid[:un] = "* enter a valid username" : @valid[:un] == ""
     params[:password] == "" ? @valid[:pw] = "* enter a valid password" : @valid[:pw] == ""
     
@@ -46,9 +73,30 @@ class ApplicationController < Sinatra::Base
 
     if @result
       # create Hiker and save to database
-      erb :'hiker/show'
+      @hiker = Hiker.find_by(username: params[:username])
+      if @hiker == nil
+        @h = Hiker.create(params)
+        redirect("/hikers/#{@h.id}")
+      else
+        erb :signup
+      end
     else
       erb :signup
+    end
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:hiker_id]
+    end
+
+    def current_user
+      User.find(session[:hiker_id])
     end
   end
 end
--- app/controllers/hikers_controller.rb
+++ app/controllers/hikers_controller.rb
@@ -1,3 +1,8 @@
 class HikersController < ApplicationController
   
+
+  get "/hikers/:id" do
+    @hiker = Hiker.find(session[:hiker_id])
+    erb :'hikers/show'
+  end
 end
