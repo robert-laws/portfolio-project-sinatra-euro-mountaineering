382c9df9f55cd7eda1c67c254f72f9a85724642c
robert-laws <roblaws@me.com>
2017-11-11 14:43:29 +0300

    hiker profile - edit complete

--- app/controllers/hikers_controller.rb
+++ app/controllers/hikers_controller.rb
@@ -9,7 +9,22 @@ class HikersController < ApplicationController
   end
 
   get "/hikers/edit" do
-    erb :'hikers/edit_hiker'
+    if logged_in?
+      @hiker = Hiker.find(session[:hiker_id])
+      erb :'hikers/edit_hiker'
+    else
+      redirect("/login")
+    end
+  end
+
+  post "/hikers/edit" do
+    if params[:first_name] == "" || params[:last_name] == "" || params[:email] == ""
+      redirect("/hikers/edit")
+    else
+      @hiker = Hiker.find(session[:hiker_id])
+      @hiker.update(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], club_id: params[:club_id])
+      redirect("/hikers")
+    end
   end
 
   get "/hikers/show/:mountain_id" do
